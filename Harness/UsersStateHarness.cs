33a08a8520fef1a1cb7580ef43b3bacbeb1e7c95
kevin.bowers <kevin.bowers@fathym.com>
2019-07-24 11:08:51 -0600

    fixing selected map issue

--- Harness/UsersStateHarness.cs
+++ Harness/UsersStateHarness.cs
@@ -67,7 +67,7 @@ namespace AmblOn.State.API.Users.Harness
             if (state.UserMaps.IsNullOrEmpty())
                 await AddCuratedMap();
 
-            if (state.SelectedMapID.IsEmpty())
+            if (state.SelectedMapID.IsEmpty() || !state.UserMaps.Any(x => x.ID == state.SelectedMapID))
                 state.SelectedMapID = state.UserMaps.First().ID;
 
             state.SelectedMapLocations = await amblGraph.ListMapLocations(details.Username, state.SelectedMapID);
