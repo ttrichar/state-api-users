820abff67ef882f5c84b1c29f34b4c33361a8cba
Matt Brooks <matt.brooks@fathym.com>
2019-10-23 09:52:04 -0600

    Left out a file. Doh.

---
+++ DedupLocations.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using AmblOn.State.API.Users.Models;
+using AmblOn.State.API.Users.Harness;
+using Microsoft.WindowsAzure.Storage;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+
+namespace AmblOn.State.API.Users
+{
+    [DataContract]
+    public class DedupLocationRequest
+    {
+        [DataMember]
+        public virtual Guid MapID { get; set; }
+    }
+    public static class DedupLocations
+    {
+        [FunctionName("DedupLocations")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Admin, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return await req.Manage<DedupLocationRequest, UsersState, UsersStateHarness>(log, async (mgr, reqData) =>
+            {
+                await mgr.DedupLocationsByMap(reqData.MapID);
+
+                return await mgr.WhenAll(
+                );
+            });
+        }
+    }
+}
