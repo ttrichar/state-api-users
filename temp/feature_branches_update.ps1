b7c89df05973c15a9ddadec587a1019e39ef6af3
AzureDevops <devops@fathym.com>
2020-05-26 01:32:16 +0000

    updated feature/BuildFix with integration

---
+++ temp/feature_branches_update.ps1
@@ -0,0 +1,34 @@
+﻿git fetch --all
+git checkout integration
+git status 
+git pull
+
+$branches = git branch -r -l
+Write-Host $branches
+
+foreach ($b in $branches)
+{
+  if($b.Contains("feature"))
+  {
+    $origin = $b.Trim()
+    $featurename = $origin.Replace('origin/', '')
+    git checkout $featurename
+    git status
+    git pull
+    git merge integration
+    $status = git status
+    if($status.Contains("conflict"))
+    {
+        git merge --abort
+        #send out notification
+    }
+    else
+    {
+        git add .
+        git commit -m "updated $featurename with integration"
+        git push origin
+        Write-Host "merged integration into $featurename"    
+    } 
+  }
+    
+}
