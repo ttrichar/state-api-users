8bfc40b9fcf2d6f6a10f2a08a28edcc2dce2d26d
Trevor Richardson <trevor.richardson@fathym.com>
2020-07-07 09:44:51 -0600

    Hot fix locations

--- Graphs/AmblOnGraph.cs
+++ Graphs/AmblOnGraph.cs
@@ -143,11 +143,15 @@ namespace AmblOn.State.API.Users.Graphs
                         Status = Status.Success
                     };
                 }
-                else
+                else{
+                    var editResp = await EditActivity(email, entAPIKey, activity);
+                
                     return new BaseResponse<Guid>() { 
                         Model = existingActivity.ID.Value,
-                        Status = Status.Conflict.Clone("An activity with that title already exists for this user's itinerary and activity group.")
+                        //Status = Status.Conflict.Clone("An activity with that title already exists for this user's itinerary and activity group.")
+                        Status = Status.Success
                     };
+                }
             });
         }
 
@@ -2055,6 +2059,49 @@ namespace AmblOn.State.API.Users.Graphs
                 totalResults.AddRange(otherResults.ToList());
 
                 return totalResults;
+            });
+        }
+
+        public virtual async Task<List<Location>> ListLocations(string email, string entAPIKey)
+        {
+            return await withG(async (client, g) =>
+            {
+                var userId = await ensureAmblOnUser(g, email, entAPIKey);
+
+                // var query = g.V(userId)
+                //     .Out(AmblOnGraphConstants.OwnsEdgeName)
+                //     .HasLabel(AmblOnGraphConstants.LayerVertexName)
+                //     .Has(AmblOnGraphConstants.IDPropertyName, layerID)
+                //     .Out(AmblOnGraphConstants.ContainsEdgeName)
+                //     .HasLabel(AmblOnGraphConstants.LocationVertexName);
+
+                var query2 = g.V(userId)
+                    .Out(AmblOnGraphConstants.OwnsEdgeName)
+                    .HasLabel(AmblOnGraphConstants.LocationVertexName);
+
+                // var results = await Submit<Location>(query);               
+
+                // if (results.ToList().Count == 0)
+                // {
+                //     query = g.V(userId)
+                //         .Out(AmblOnGraphConstants.OwnsEdgeName)
+                //         .HasLabel(AmblOnGraphConstants.SharedLayerVertexName)
+                //         .Has(AmblOnGraphConstants.IDPropertyName, layerID)
+                //         .Out(AmblOnGraphConstants.InheritsEdgeName)
+                //         .HasLabel(AmblOnGraphConstants.LayerVertexName)
+                //         .Out(AmblOnGraphConstants.ContainsEdgeName)
+                //         .HasLabel(AmblOnGraphConstants.LocationVertexName);
+
+                //     results = await Submit<Location>(query);                  
+                // }
+                //Query to return locations directly associated with AmblOnUser account
+                var otherResults = await Submit<Location>(query2);
+
+                // var totalResults = results.ToList();
+
+                // totalResults.AddRange(otherResults.ToList());
+
+                return otherResults.ToList();
             });
         }
 
